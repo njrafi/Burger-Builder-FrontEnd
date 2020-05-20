5e4cf8429f0beb6debf345fa5dd888915c10407b
Nayeem Jahan Rafi <njrafibd@gmail.com>
2020-05-20 21:45:34 +0600

    Initial Setup Done

---
+++ src/Containers/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./App.module.css";
+
+function App() {
+	return (
+		<div className={styles.App}>
+			<p>Hello World</p>
+		</div>
+	);
+}
+
+export default App;
