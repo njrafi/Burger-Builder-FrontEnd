8432d009acf56f8d509de262a8cb096bf9cbcc88
Nayeem Jahan Rafi <njrafibd@gmail.com>
2020-05-22 16:05:44 +0600

    Navigation Item Added

--- src/Components/Navigation/Toolbar/Toolbar.js
+++ src/Components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import styles from "./Toolbal.module.css";
 import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
 
 const Toolbar = (props) => {
 	return (
 		<header className={styles.Toolbar}>
 			<div>MENU</div>
 			<Logo />
-			<nav>....</nav>
+			<NavigationItems />
 		</header>
 	);
 };
