535fd52a7fb30603c1a509ee0d34dcb9756e640e
Nayeem Jahan Rafi <njrafibd@gmail.com>
2020-05-22 12:39:47 +0600

    added Price

--- src/Components/Burger/BuildControls/BuildControls.js
+++ src/Components/Burger/BuildControls/BuildControls.js
@@ -23,6 +23,9 @@ const controls = [
 const BuildControls = (props) => {
 	return (
 		<div className={styles.BuildControls}>
+			<p>
+				Current Price: <strong>{props.price.toFixed(2)} $</strong>
+			</p>
 			{controls.map((ctrl, index) => {
 				return (
 					<BuildControl
