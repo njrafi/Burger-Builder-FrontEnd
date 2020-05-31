b88224991e6586389f795811b4c798844ad39b17
Nayeem Jahan Rafi <njrafibd@gmail.com>
2020-06-01 02:52:31 +0600

    Order Span Added

--- src/Components/Order/Order.js
+++ src/Components/Order/Order.js
@@ -2,13 +2,31 @@ import React from "react";
 import styles from "./Order.module.css";
 
 const Order = (props) => {
+	let ingredients = [];
+	for (let ingredientName in props.ingredients)
+		ingredients.push({
+			name: ingredientName,
+			amount: props.ingredients[ingredientName],
+		});
+
+	const ingredientOutput = ingredients.map((ingredient) => {
+		return (
+			<span
+				style={{
+					textTransform: "capitalize",
+					display: "inline-block",
+					margin: "0 8px",
+					border: "1px solid #ccc",
+					padding: "5px",
+				}}
+			>
+				{ingredient.name} ({ingredient.amount})
+			</span>
+		);
+	});
 	return (
 		<div className={styles.Order}>
-			<p>
-				Ingredients: Salad({props.ingredients.salad}) Cheese(
-				{props.ingredients.cheese}) Meat({props.ingredients.meat}) Bacon(
-				{props.ingredients.bacon})
-			</p>
+			<p>Ingredients: {ingredientOutput}</p>
 			<p>
 				Price: <strong>{props.price.toFixed(2)} $</strong>
 			</p>
